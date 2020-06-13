6164c68e3426618cac179d371d26ce2478e986d0
Riya Singh <43998588+riyasingh26@users.noreply.github.com>
2020-06-14 03:19:20 +0530

    Update Kadane_Algorithm.java

--- Kadane_Algorithm.java
+++ Kadane_Algorithm.java
@@ -1,5 +1,6 @@
 package demo;
 import java.util.Scanner;
+import java.lang.*;
 public class kadane_algorithm {
 
 	public static void main(String[] args) {
@@ -18,18 +19,13 @@ public class kadane_algorithm {
 	}
 	
 	public static int kadane(int n, int[]arr) {
-		int meh = 0;
-		int msf = 0;
-		for(int i = 0;i<n;i++) {
-			meh = meh + arr[i];
-			if(meh<0) {
-				meh = 0;
-			}
-			if(msf<meh) {
-				msf = meh;
-			}
+		int max = arr[0];
+		int current_sum = arr[0];
+		for(int i = 1; i < n; i++){
+		current_sum = Math.max(arr[i], current_sum+arr[i]);
+		max = Math.max(max, current_sum);
 		}
-		return msf;
+		return max;
 	}
 
 }
